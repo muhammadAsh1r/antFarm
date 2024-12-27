2f9183229e4aa8748e5ad9dccfd01563b198f90c
Muhammad Ashir Khan <just4fun17678@gmail.com>
2024-12-27 20:27:21 +0500

    Make a CLI based menu for User Interaction

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,65 +1,124 @@
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <string>
 using namespace std;
 
-class Ant{
+class Ant
+{
     int number_of_ants = 0;
     int number_of_ants_resting = 0;
     string queen;
     string pawn;
-    public:
-    void do_battle(){
-        if(0){
+
+public:
+    void do_battle()
+    {
+        if (0)
+        {
             cout << "Batlle Won!";
         }
     }
 
-    void rest() {
+    void rest()
+    {
         cout << "Ant resting" << endl;
     }
 
-    void consume_food(){
+    void consume_food()
+    {
         cout << "Ant consuming food" << endl;
     }
 
-    void start_construction(){
-            cout << "Ant constructing" << endl;        
+    void start_construction()
+    {
+        cout << "Ant constructing" << endl;
     }
 };
 
-class AntFarm{
-    private:
-        string species;
-        vector <Ant> ants;
-    public:
-        AntFarm(string sp){
-            species = sp;
-        }
+class AntFarm
+{
+private:
+    string species;
+    vector<Ant> ants;
 
-        void addAnt(Ant &ant){
-            ants.push_back(ant);
-        }
+public:
+    AntFarm(string sp)
+    {
+        species = sp;
+    }
 
-        void performTick(){
-            ants->start_construction();
-        }
+    void addAnt(Ant &ant)
+    {
+        ants.push_back(ant);
+    }
+
+    void performTick()
+    {
+        // ants->start_construction();
+    }
 };
 
-class Meadow{
+class Meadow
+{
 private:
     static Meadow *temp;
     vector<AntFarm> farm;
+
 public:
-    Meadow(){
-        AntFarm farms = new Meadow();
+    Meadow()
+    {
+        // AntFarm farms = new Meadow();
     }
 
-    void addAntFarm(shared_ptr<AntFarm> farm) {
-        antFarms.push_back(farm);
+    void addAntFarm(AntFarm farm)
+    {
+        // antFarms.push_back(farm);
     }
 };
 
-int main() {
-  
-  return 0;
+int main()
+{
+    string command;
+    cout << "Ant Farm Simulation started.\n";
+    cout << "Available commands:\n";
+    cout << "1. spawn X Y Species - Create a new colony.\n";
+    cout << "2. tick [T] - Simulate T ticks.\n";
+    cout << "3. give I R A - Allocate resources or ants to a colony.\n";
+    cout << "4. summary I - Show colony summary.\n";
+    cout << "5. exit - End the simulation.\n";
+
+    while (true)
+    {
+        cout << "Enter command: ";
+        getline(cin, command);
+
+        if (command == "exit")
+        {
+            cout << "Exiting the simulation...\n";
+            break;
+        }
+        else if (command.rfind("spawn", 0) == 0)
+        {
+            cout << "Spawning an ant farm...\n";
+        }
+        else if (command.rfind("tick", 0) == 0)
+        {
+            cout << "Simulating ticks...\n";
+        }
+        else if (command.rfind("summary", 0) == 0)
+        {
+            cout << "Showing summary...\n";
+        }
+        else if (command.rfind("give", 0) == 0)
+        {
+            cout << "Giving resources or ants...\n";
+        }
+        else
+        {
+            cout << "Unknown command.\n";
+        }
+    }
+
+    return 0;
+    return 0;
 }
