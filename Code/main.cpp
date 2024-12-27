3a65a799258ec61218f33f256c010611a189a57b
Muhammad Ashir Khan <just4fun17678@gmail.com>
2024-12-27 20:02:03 +0500

    Added Ant class with attributes

--- Code/main.cpp
+++ Code/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class Ant{
+    int number_of_ants;
+    string queen;
+    string pawn;
+    public:
+    void do_battle(){
+        if(0){
+            cout << "Batlle Won!";
+        }
+    }
+};
+
 int main() {
   cout << "Hello World!";
   return 0;
